528f494297e362bef6ded3449c327e5d8a882017
David Benavidez <benavidez_david13@yahoo.com>
2019-06-10 01:36:00 -0700

    updated database

--- knexfile.js
+++ knexfile.js
@@ -1,20 +1,28 @@
 // Update with your config settings.
+const pg = require('pg');
+
+const dbConnection = process.env.DATABASE_URL;
 
 module.exports = {
-
-    development: {
-      client: 'sqlite3',
-      connection: {
-        filename: './db/dev.sqlite3'
-      },
-      useNullAsDefault: true,
-      migrations: {
-        directory: "./db/migrations",
-        tableName: 'dbmigrations',
-      },
+  development: {
+    client: 'sqlite3',
+    connection: {
+      filename: './db/dev.sqlite3'
     },
-  
-    
-  
-  };
-  
+    useNullAsDefault: true,
+    migrations: {
+      directory: './db/migrations',
+      tableName: 'dbmigrations'
+    }
+  },
+  production: {
+    client: 'pg',
+    connection: {
+      dbConnection
+    },
+    migrations: {
+      directory: './db/migrations',
+      tableName: 'dbmigrations'
+    }
+  }
+};
